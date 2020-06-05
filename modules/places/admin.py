024cd473114a9c0a133989e570a528bf21e36050
Artem <ufimzev.art@gmail.com>
2020-06-05 18:52:44 +0700

    Add preview for place in admin

--- modules/places/admin.py
+++ modules/places/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Place, PlacePhoto
 
 
 class PlacePhotoInline(admin.TabularInline):
     model = PlacePhoto
-    fields = ('photo', 'order')
+    fields = ('photo', 'get_preview', 'order')
+    readonly_fields = ('get_preview',)
     extra = 1
+
+    @staticmethod
+    def get_preview(obj):
+        return mark_safe(f'<img height="200" src="{obj.photo.url}" />')
 
 
 @admin.register(PlacePhoto)
